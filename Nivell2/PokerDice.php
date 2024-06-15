b041a657c761e93bdf6a395714d382fd9e09e4cd
[Sergi López] <socelsergi@gmail.com>
2024-06-15 21:09:30 +0200

    till thow

--- Nivell2/PokerDice.php
+++ Nivell2/PokerDice.php
@@ -1,7 +1,20 @@
 <?php
 class pokerDice{
-    public $As;
-    public 
+    public static $faces = ["As", "K", "Q", "J", "7", "8"];
+    public static $totalThrows = 0;
+    public $currentFace;
+    public $throws;
+
+
+    public function __construct(){
+        $this->currentFace = null;
+        $this->throws = 0;
+    } 
+
+    public function throw(){
+        $this->currentFace = self::$faces[array_rand(self::$faces)];
+        self::$totalThrows++;
+    }
 }
 
 ?>
